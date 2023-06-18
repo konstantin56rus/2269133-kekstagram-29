ad94e0bc90417c02ebfd41e4896af0c9506530df
konstantin56rus <konstantin56rus@list.ru>
2023-06-18 10:51:53 +0300

    Добавляет первый код в js

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const isFormValidation = (str, num) => str.lenght <= num;
+
+isFormValidation('bourbon', 7);
+
+const isPalindrome = (str) => str.toUpperCase() === str.toUpperCase().split('').reverse().join('');
+
+isPalindrome('madam');
